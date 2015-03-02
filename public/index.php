281a1bf9890371119e09863996593bd019b093d4
Christopher Schmitt <cs@konekobox.de>
2015-03-02 20:47:25 +0100

    whuups forgot to commit the SlimController initialisation

--- public/index.php
+++ public/index.php
@@ -2,12 +2,12 @@
 
 require '../vendor/autoload.php';
 
-$app = new \Slim\Slim([
-    //'debug' => false,
+$app = new \SlimController\Slim([
+    'controller.class_prefix'    => '\\konekobox\\chisai\\controllers',
+    'controller.method_suffix'   => 'Action',
     'view' => new \Slim\Views\Twig(),
     'templates.path' => '../app/views/'
 ]);
-
 
 
 require_once '../app/bootstrap.php';
