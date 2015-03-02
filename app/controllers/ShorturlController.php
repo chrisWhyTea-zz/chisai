b6413df48bc2d664d262557a7a09b77968e88d57
Christopher Schmitt <cs@konekobox.de>
2015-03-02 20:45:04 +0100

    moved the logic from the route closures to the new ShorturlController (With a proper repository pattern...)

---
+++ app/controllers/ShorturlController.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace konekobox\chisai\controllers;
+
+use konekobox\chisai\repositories\ShorturlRepository;
+use SlimController\SlimController;
+
+class ShorturlController extends SlimController {
+
+    private $Shorturl;
+
+    function __construct(\Slim\Slim &$app)
+    {
+
+        parent::__construct($app);
+        $this->Shorturl = new ShorturlRepository();
+    }
+
+
+    public function indexAction(){
+        $this->render('index.twig');
+    }
+
+    public function createAction()
+    {
+        $shorttag = $this->Shorturl->create($this->app->request->post('url'));
+        $this->app->redirect("/$shorttag/details");
+    }
+
+    public function redirectAction($shorttag){
+        $shorturl = $this->Shorturl->getByShort($shorttag);
+        $this->Shorturl->statisticVisit($shorturl);
+        $this->app->redirect($shorturl->url);
+    }
+
+    public function detailsAction($shorttag){
+        $shorturlDetails = $this->Shorturl->getByShort($shorttag);
+
+        $this->render('details.twig', [
+            "url" => $shorturlDetails->url,
+            "visitors" => $shorturlDetails->visitors,
+            "created_at" => $shorturlDetails->created_at,
+            "last_visited_at" => $shorturlDetails->last_visited_at,
+            "short" => $shorturlDetails->short,
+        ]);
+    }
+    
+}
--- app/routes.php
+++ app/routes.php
@@ -1,41 +1,17 @@
 <?php
-/**
- * Render the Index page
- */
-$app->get('/', function () use ($app) {
-    $app->render('index.html');
-});
 
-/**
- * Create new Shorturl
- */
-$app->post('/', function () use ($app) {
-    $short = $app->shorturlRepo->create($app->request->post('url'));
-    $app->redirect("/$short/details");
-});
 
-/**
- * Using the shorturl to redirect to the real url
- */
-$app->get('/:short', function ($short) use ($app) {
-    $shorturl = $app->shorturlRepo->getByShort($short);
-    $app->shorturlRepo->statisticVisit($shorturl);
-    $app->redirect($shorturl->url);
-
-});
-
-/**
- * Show some details for the shorturl
- */
-$app->get('/:short/details', function ($short) use ($app) {
-
-    $shorturlDetails = $app->shorturlRepo->getByShort($short);
-
-    $app->render('details.html', [
-        "url" => $shorturlDetails->url,
-        "visitors" => $shorturlDetails->visitors,
-        "created_at" => $shorturlDetails->created_at,
-        "last_visited_at" => $shorturlDetails->last_visited_at,
-        "short" => $shorturlDetails->short,
-    ]);
-});
+$app->addRoutes([
+    '/' => [
+        'get' => [
+            'ShorturlController:index',
+            function () {}
+        ],
+        'post' => [
+            'ShorturlController:create',
+            function () {}
+        ]
+    ],
+    '/:short' => 'ShorturlController:redirect',
+    '/:short/details' => 'ShorturlController:details'
+]);
